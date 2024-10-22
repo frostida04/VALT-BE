df93cb367bbdf460badfda352aeb9adda41ba13c
0xHarry2 <0xharry517@gmail.com>
2024-10-22 11:03:57 -0400

    updated

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const mongoose = require("mongoose");
 // const bodyParser = require('body-parser');
 const addevent = require("./routes/addevent");
@@ -7,7 +7,7 @@ const addorganizers = require("./routes/addorganizers");
 const admin = require("./routes/addevent");
 const prevHome = require("./routes/prevHome");
 const prevAdmin = require("./routes/prevAdmin");
-const multer = require("multer");
+// const multer = require("multer");
 
 require("dotenv").config();
 
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json())
 app.use(cors());
 app.use(express.urlencoded(({ extended: true })));
-app.use("/images", express.static(path.join(__dirname, "/images")));
+// app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose.connect(process.env.MONGODB_URI , {
     dbName: "VALTdatabase",
@@ -31,20 +31,20 @@ database.once("connected", () => {
     console.log("Database Connected");
 });
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "/images");
-    },
-    filename: (req, file, cb) => {
-        cb(null, req.body.name);
-    },
-});
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, "/images");
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, req.body.name);
+//     },
+// });
 
-const upload = multer({ storage: storage });
-app.post("/upload", upload.single("file"), (req, res) => {
-    const imageUrl = `http://localhost:${port}/uploads/${req.file.filename}`;
-    res.status(200).json({imageUrl});
-});
+// const upload = multer({ storage: storage });
+// app.post("/upload", upload.single("file"), (req, res) => {
+//     const imageUrl = `http://localhost:${port}/uploads/${req.file.filename}`;
+//     res.status(200).json({imageUrl});
+// });
 
 
 
